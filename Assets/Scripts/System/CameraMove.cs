29f084cfba92dbf0b97199599ec4b781cd0711d5
T-WK <66228633+T-WK@users.noreply.github.com>
2025-12-24 01:08:30 +0900

    카메라 움직임 구현
    - 카메라가 유저를 따라감
    
    - 카메라에 줌 IN, OUT 기능 구현함

---
+++ Assets/Scripts/System/CameraMove.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraMove : MonoBehaviour
+{
+    public Vector3 offset = new Vector3(0, 20, -30);
+    public float zoomSpeed = 1f;
+
+    private void Awake()
+    {
+        // camera_Actions = new Camera_Actions();
+    }
+
+    public void OnMouseScrollY(InputValue value)
+    {
+        float zoomDelta = value.Get<float>() * zoomSpeed;
+        offset.y -= zoomDelta * 1f;
+        offset.z += zoomDelta * 1.5f;
+
+        offset.y = Mathf.Clamp(offset.y, 1f, 20f);
+        offset.z = Mathf.Clamp(offset.z, -30f, -1.5f);
+    }
+
+    void Update()
+    {
+        transform.position = TargetProvider.Player.position + offset;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = TargetProvider.Player.position + offset;
+    }
+}
